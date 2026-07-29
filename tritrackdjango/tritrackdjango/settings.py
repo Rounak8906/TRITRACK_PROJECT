47cab6be2726616a7a35281b60c8fe90b3f41b32
Rounak8906 <rounaksingh8906@gmail.com>
2026-07-30 01:58:53 +0530

    Update settings.py

--- tritrackdjango/tritrackdjango/settings.py
+++ tritrackdjango/tritrackdjango/settings.py
@@ -5,7 +5,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.environ.get('SECRET_KEY', 'tritrack-secret-key-123-xyz')
 
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
