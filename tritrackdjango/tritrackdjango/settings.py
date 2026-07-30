5c2909ceb9756cdaf847e5896fb6104d3fe0d76a
Rounak8906 <rounaksingh8906@gmail.com>
2026-07-30 13:18:35 +0530

    Update settings.py

--- tritrackdjango/tritrackdjango/settings.py
+++ tritrackdjango/tritrackdjango/settings.py
@@ -36,7 +36,7 @@ ROOT_URLCONF = 'tritrackdjango.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'core' / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
